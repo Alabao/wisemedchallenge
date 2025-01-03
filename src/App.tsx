64f275dff6670acbdfbdf45c8cf4da9b34bac6cb
Rubier Sorio <rubiersorio2014@gmail.com>
2025-01-02 21:46:30 -0500

    Ajustando a columnas

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,19 @@ function App() {
     return (
         <div className="container">
             <h1 className="title">Listado de Pacientes</h1>
-            <PatientCard name="Juan Perez" age={25} diagnostics="Fiebre"
-                         onButtonClick={handlerDetailsOnClick}/>
-            <PatientCard name="Pedro Gomez" age={25}
-                         diagnostics="Malestar general"
-                         onButtonClick={handlerDetailsOnClick}/>
+            <div className="columns">
+                <div className="column">
+                    <PatientCard name="Juan Perez" age={25} diagnostics="Fiebre"
+                                 onButtonClick={handlerDetailsOnClick}/>
+                </div>
+                <div className="column">
+                    <PatientCard name="Pedro Gomez" age={25}
+                                 diagnostics="Malestar general"
+                                 onButtonClick={handlerDetailsOnClick}/>
+                </div>
+            </div>
+
+
         </div>
     )
 }
